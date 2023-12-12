c63beb67439aa8b277bec408eb5bdb9fa0f67b3b
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-12-12 12:53:45 +0700

    fix(types): fix engine name inferring

--- src/types.ts
+++ src/types.ts
@@ -31,7 +31,12 @@ export type Engine =
       parse: (res: Response) => string | Promise<string>
     }
 
-export interface Options<CustomEngines extends Record<string, Engine> = Record<string, Engine>> {
+export interface Options<
+  Engines extends Record<string, Engine> = Record<string, Engine>,
+  EngineName extends keyof typeof defaultEngines | keyof Engines =
+    | keyof typeof defaultEngines
+    | keyof Engines,
+> {
   /** source language - default: 'en' */
   from?: LiteralUnion<LanguageCode, string>
   /** target language - default: 'en' */
@@ -41,9 +46,9 @@ export interface Options<CustomEngines extends Record<string, Engine> = Record<s
   key?: string
 
   /** translation engine name - default: 'google' */
-  engine?: keyof typeof defaultEngines | keyof CustomEngines
+  engine?: EngineName
   /** custom engines definition */
-  engines?: CustomEngines
+  engines?: Engines
 
   /** custom url for specific engines */
   url?: string
